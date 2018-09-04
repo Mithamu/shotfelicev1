be781021e10599af714f64451d72cd7c83e84500
mir32 <mir32@example.com>
2018-09-04 17:18:47 +0900

    Automated commit at 2018-09-04 08:18:47 UTC by middleman-deploy 2.0.0-alpha

--- assets/js/jquery_script.js
+++ assets/js/jquery_script.js
@@ -1,1 +1,1 @@
-(function(){$(function(){var n;return $(window).load(function(){return $("#cover").fadeOut()}),n=$("#page_top"),n.hide(),$(window).scroll(function(){return $(this).scrollTop()>400?n.fadeIn(300):n.fadeOut(300)})})}).call(this);
+(function(){$(function(){var n;return $(window).load(function(){return $("#cover").fadeOut()}),alert($("body").width),n=$("#page_top"),n.hide(),$(window).scroll(function(){return $(this).scrollTop()>400?n.fadeIn(300):n.fadeOut(300)})})}).call(this);
