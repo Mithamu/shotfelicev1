6093bebcbc9553caf0e25d4b69a17b3f165ceb9b
mir32 <mir32@example.com>
2018-09-05 04:41:11 +0900

    Automated commit at 2018-09-04 19:41:11 UTC by middleman-deploy 2.0.0-alpha

--- assets/js/jquery_script.js
+++ assets/js/jquery_script.js
@@ -1,1 +1,1 @@
-(function(){$(function(){var n;return $(window).load(function(){return $("#cover").fadeOut()}),n=$("#page_top"),n.hide(),$(window).scroll(function(){return $(this).scrollTop()>400?n.fadeIn(300):n.fadeOut(300)})})}).call(this);
+(function(){$(function(){var n;return $(window).load(function(){return $(".load").fadeOut()}),n=$(".page_top_button"),n.hide(),$(window).scroll(function(){return $(this).scrollTop()>400?n.fadeIn(300):n.fadeOut(300)})})}).call(this);
